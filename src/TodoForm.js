f84f25a414118458c965cd81af0e749535b84859
tynail <marc4283@hotmail.com>
2020-06-10 12:22:00 -0400

    create custom hooks with reducer for localStorage

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,11 +1,11 @@
 import React, { useContext } from "react";
 import { Paper, TextField } from "@material-ui/core";
 import useInputState from "./hooks/useInputState";
-import { TodosContext } from "./contexts/todos.context";
+import { DispatchContext } from "./contexts/todos.context";
 const TodoForm = () => {
   const [value, handleChange, reset] = useInputState("");
   // Consumet Subscribe to the Provider to get what he need, in this case addTodo
-  const dispatch = useContext(TodosContext);
+  const dispatch = useContext(DispatchContext);
   return (
     <Paper style={{ margin: "1rem 0", padding: "0 1rem" }}>
       <form
--- src/contexts/todos.context.js
+++ src/contexts/todos.context.js
@@ -1,6 +1,7 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext } from "react";
 import todoReducer from "../reducers/todo.reducer";
-const defaultTodo = [
+import useLocalStorageReducer from "../hooks/useLocalStorageReducer";
+const defaultTodos = [
   { id: 1, task: "Do more Programmation", completed: false },
   { id: 3, task: "Do more Exercice", completed: false },
 ];
@@ -8,7 +9,11 @@ export const TodosContext = createContext();
 export const DispatchContext = createContext();
 
 export function TodosProvider(props) {
-  const [todos, dispatch] = useReducer(todoReducer, defaultTodo);
+  const [todos, dispatch] = useLocalStorageReducer(
+    "todos",
+    defaultTodos,
+    todoReducer
+  );
   return (
     <TodosContext.Provider value={todos}>
       <DispatchContext.Provider value={dispatch}>
